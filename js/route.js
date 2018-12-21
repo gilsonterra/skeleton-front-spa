276f6a58a90a0085d79b23e90de555a199c278ba
Gilson Terra <gilsonterra@gmail.com>
2018-12-21 11:49:35 -0200

    docs

--- js/route.js
+++ js/route.js
@@ -1,23 +1,23 @@
 route('/', function () {
-    APP.load.js('/tags/home.js').then(function () {        
+    APP.load.js('tags/home.js').then(function () {        
         riot.mount('#view', 'home');        
     });
 });
 
 route('/home', function () {
-    APP.load.js('/tags/home.js').then(function () {
+    APP.load.js('tags/home.js').then(function () {
         riot.mount('#view', 'home', {'attrs' : {'placeholder': 'Isso é um teste', 'maxlength': '2'}});        
     });    
 });
 
 route('/incident', function () {    
-    APP.load.js('/tags/incident.js').then(function () {
+    APP.load.js('tags/incident.js').then(function () {
         riot.mount('#view', 'incident'); 
     });
 });
 
 route('/clothes', function () {
-    APP.load.js('/tags/clothes.js').then(function () {
+    APP.load.js('tags/clothes.js').then(function () {
         Request.get('data/ladies_outerwear.json', function (json) {
             riot.mount('#view', 'clothes', {
                 'items': json,
@@ -28,7 +28,7 @@ route('/clothes', function () {
 });
 
 route('/clothes/*', function (uri) {
-    APP.load.js('/tags/clothes.js').then(function () {
+    APP.load.js('tags/clothes.js').then(function () {
         Request.get('data/' + uri + '.json', function (json) {
                 riot.mount('#view', 'clothes', {
                     'items': json,
